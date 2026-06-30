a8490276ec928abe320768145d827b16792fdd8e
zxj <365712688@qq.com>
2026-06-30 18:18:38 +0800

    随便提交一个

---
+++ Test01.java
@@ -0,0 +1,12 @@
+/**
+  * @Description 
+  * @Author zxj
+  * @Date 2026-06-30 18:18:03
+  **/public class Test01 {
+
+
+    public static void main(String[] args) {
+        String str="1";
+        System.out.println(str);
+    }
+}
